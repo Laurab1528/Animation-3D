03ca44d76f5979c75ac5cc824bcbd7c7648749e5
Laura Bermudez <laurab1528@hotmail.com>
2024-08-16 18:48:16 -0500

    entrega16

--- Assets/Scripts/Character/CharacterVisualController.cs
+++ Assets/Scripts/Character/CharacterVisualController.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 public class CharacterVisualController : MonoBehaviour
 {
     public event Action OnAttackAnimationFinished;
-    
+
     private Animator animator;
+    private ParticleSystem runningParticleSystem;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        runningParticleSystem = GetComponentInChildren<ParticleSystem>(); // Asigna automáticamente el sistema de partículas si está en un hijo
+    }
+
+    private void Update()
+    {
+        // Obtén el estado del parámetro IsRunning y actualiza el sistema de partículas.
+        bool isRunning = animator.GetBool("IsRunning");
+        UpdateParticleSystem(isRunning);
     }
 
     public void SetBoolAnimation(string animationParamName, bool parameterState)
     {
-        if(animator.GetBool(animationParamName) != parameterState)
-            animator.SetBool(animationParamName, parameterState);
+        animator.SetBool(animationParamName, parameterState);
     }
 
     public void SetTriggerAnimation(string animationParamName)
@@ -26,5 +34,26 @@ public class CharacterVisualController : MonoBehaviour
     public void RaiseFinishAttackAnimation()
     {
         OnAttackAnimationFinished?.Invoke();
+    }
+
+    private void UpdateParticleSystem(bool isRunning)
+    {
+        if (runningParticleSystem != null)
+        {
+            if (isRunning && !runningParticleSystem.isPlaying)
+            {
+                runningParticleSystem.Play(); // Activa el sistema de partículas
+                Debug.Log("Particle System Started");
+            }
+            else if (!isRunning && runningParticleSystem.isPlaying)
+            {
+                runningParticleSystem.Stop(); // Desactiva el sistema de partículas
+                Debug.Log("Particle System Stopped");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Running Particle System not assigned.");
+        }
     }
 }
